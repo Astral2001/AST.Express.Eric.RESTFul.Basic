1a8ab73e9176a8fa75fff8ebe167ba08fa34a6c0
Lại An Nguyên <83880862+Astral2001@users.noreply.github.com>
2024-01-16 01:03:35 +0700

    #16. Express Activity Model

--- starter/index.js
+++ starter/index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express') // commonJS
+const app = express() // app express
+const port = 8080 // port
 
+// route app
 app.get('/', (req, res) =>{
     res.send('Hello World!')
 })
 
+// listen port
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
