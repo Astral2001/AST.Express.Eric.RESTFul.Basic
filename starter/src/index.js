29b70db67924c09ecb6b7119cbb27b39be60a436
Lại An Nguyên <83880862+Astral2001@users.noreply.github.com>
2024-01-16 23:20:35 +0700

    #30. MySQL with NodeJS

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config() // dotenv
 
 const express = require('express') // commonJS
+const mysql = require('mysql2') // commonJS
 
 // require configs
 const configViewEngine = require('./configs/viewEngine')
@@ -17,6 +18,24 @@ const port = process.env.PORT ?? 8000// port
 // config
 configViewEngine(app) // view engine
 configStaticFiles(app) // static files
+
+// test connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+})
+
+connection.query(
+    'SELECT * FROM Users',
+    (err, results, fields) => {
+        console.log(err)
+        console.log(results) // results contains rows returned by server
+        console.log(fields) // fields contains extra meta data about results, if available
+    }
+)
 
 // routes
 app.use('/', webRoutes) // web routes
